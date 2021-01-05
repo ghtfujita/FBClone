b8e11a3dd66f5f6b5f89ea2f3a540a1d859cb12f
takeshi fujita <mail.takeshi.fujita@gmail.com>
2021-01-05 11:47:41 +0900

    image upload function implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :feeds do
+    collection do
+      post :confirm
+    end
+  end
   root 'tops#index'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
