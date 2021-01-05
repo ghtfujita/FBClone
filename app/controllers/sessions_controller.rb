f22f8b504898b277dbd6acb8bc1a11525a9c6389
takeshi fujita <mail.takeshi.fujita@gmail.com>
2021-01-05 11:13:53 +0900

    logout func. in place

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,4 +11,9 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'Logged out!'
+    redirect_to new_session_path
+  end
 end
